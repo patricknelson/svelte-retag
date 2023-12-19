d642e9ba8e3c23a6d28849e84da33595cb841906
Patrick Nelson <patnelson@ebay.com>
2023-12-18 17:31:21 -0800

    Unit tests: Nesting.test.js: Using DOM queries instead of HTML to validate nesting

--- tests/Nesting.test.js
+++ tests/Nesting.test.js
@@ -1,7 +1,7 @@
 import { describe, beforeAll, afterAll, afterEach, test, expect, vi } from 'vitest';
 import Nesting from './Nesting.svelte';
 import svelteRetag from '../index.js';
-import { normalizeWhitespace, syncRaf } from './test-utils.js';
+import { syncRaf } from './test-utils.js';
 
 let el = null;
 let debugMode = false; // set to 'cli' to see debug output.
@@ -24,19 +24,30 @@ describe('<nesting-tag> (Nesting)', () => {
 		window.requestAnimationFrame.mockRestore();
 	});
 
-	const nestedOpen = '<nesting-tag><svelte-retag><div>';
-	const nestedClose = '</div><!--<Nesting>--></svelte-retag></nesting-tag>';
-
 	test('nested (zero levels)', async () => {
 		el = document.createElement('div');
 		el.innerHTML = '<nesting-tag></nesting-tag>';
 		document.body.appendChild(el);
-		expect(normalizeWhitespace(el.innerHTML)).toBe(nestedOpen + nestedClose);
+
+		// Instead of doing direct HTML comparison, use DOM API to ensure the expected <div> is found nested under the parent.
+		const nestingTag = el.querySelector('nesting-tag');
+		const div = nestingTag.querySelector('div');
+		expect(div).not.toBe(null);
 	});
 	test('nested (one level)', async () => {
 		el = document.createElement('div');
 		el.innerHTML = '<nesting-tag><nesting-tag></nesting-tag></nesting-tag>';
 		document.body.appendChild(el);
-		expect(normalizeWhitespace(el.innerHTML)).toBe(nestedOpen + nestedOpen + nestedClose + nestedClose);
+
+		// Same as above, except we should ensure we can find the following hierarchy: nesting-tag > div > nesting-tag > div
+		// Also just verify each tag is present as we go down.
+		const topNestingTag = el.querySelector('nesting-tag');
+		expect(topNestingTag).not.toBe(null);
+		const topDiv = topNestingTag.querySelector('div');
+		expect(topDiv).not.toBe(null);
+		const nestedNestingTag = topDiv.querySelector('nesting-tag');
+		expect(nestedNestingTag).not.toBe(null);
+		const nestedDiv = nestedNestingTag.querySelector('div');
+		expect(nestedDiv).not.toBe(null);
 	});
 });
